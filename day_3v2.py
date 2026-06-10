cc478378d35005c0ec4cc6f1cc200fb45863945f
Shankar <shankarunni18@gmail.com>
2026-06-10 15:28:54 +0530

    day 3

--- day_3v2.py
+++ day_3v2.py
@@ -31,13 +31,10 @@ df.duplicated().sum()
 corr = df.corr(numeric_only=True)
 sns.heatmap(corr, annot=True)
 
-# Sales trend
 df.groupby('Date')['Weekly_Sales'].sum().plot(figsize=(12,5))
 
-# Holiday vs Non-Holiday
 sns.boxplot(x='Holiday_Flag', y='Weekly_Sales', data=df)
 
-# Store-wise sales
 df.groupby('Store')['Weekly_Sales'].sum().plot(kind='bar')
 
 plt.figure(figsize=(8,5))
@@ -105,30 +102,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-# Convert 'Date' column to datetime objects
 df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')
 
-# Extract numerical features from 'Date'
 df['Year'] = df['Date'].dt.year
 df['Month'] = df['Date'].dt.month
 df['Day'] = df['Date'].dt.day
 
-# Features and Target
-X = df.drop(["Weekly_Sales", "Date"], axis=1) # Drop original 'Date' column
+X = df.drop(["Weekly_Sales", "Date"], axis=1)
 y = df["Weekly_Sales"]
 
-# Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train Model
 rf = RandomForestRegressor(n_estimators=200,random_state=42,n_jobs=-1)
 
 rf.fit(X_train, y_train)
 
-# Predictions
 y_pred = rf.predict(X_test)
 
-# Evaluation
 mae = mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = r2_score(y_test, y_pred)
@@ -159,26 +149,27 @@ print("Highly Skewed Columns: \n",highly_skewed)
 print("Negatively Skewed Columns: \n",negatively_skewed)
 
 df[highly_skewed] = df[highly_skewed].apply(lambda x: np.log1p(x))
-from sklearn.preprocessing import PowerTransformer
-pt = PowerTransformer(method="yeo-johnson")
-df[negatively_skewed] = pt.fit_transform(df[negatively_skewed])
 
-# Features and Target
-X = df.drop(["Weekly_Sales", "Date"], axis=1) # Drop original 'Date' column
+from sklearn.preprocessing import PowerTransformer
+
+negatively_skewed_features_list = [col for col in negatively_skewed if col != 'Weekly_Sales']
+pt_features = PowerTransformer(method="yeo-johnson")
+df[negatively_skewed_features_list] = pt_features.fit_transform(df[negatively_skewed_features_list])
+
+pt_target = PowerTransformer(method="yeo-johnson")
+df['Weekly_Sales'] = pt_target.fit_transform(df[['Weekly_Sales']])
+
+X = df.drop(["Weekly_Sales", "Date"], axis=1)
 y = df["Weekly_Sales"]
 
-# Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train Model
 rf = RandomForestRegressor(n_estimators=200,random_state=42,n_jobs=-1)
 
 rf.fit(X_train, y_train)
 
-# Predictions
 y_pred = rf.predict(X_test)
 
-# Evaluation
 mae = mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = r2_score(y_test, y_pred)
@@ -195,3 +186,28 @@ plt.title("Actual Weekly Sales vs Predicted Weekly Sales")
 plt.axline([0,0],[1,1],color="red",linestyle
            ="--")
 plt.show()
+
+sample_input_data = pd.DataFrame([{
+    'Store': 1,
+    'Holiday_Flag': 1,
+    'Temperature': 38.51,
+    'Fuel_Price': 2.548,
+    'CPI':211.242170,
+    'Unemployment': 8.106,
+    'Year': 2010,
+    'Month': 2,
+    'Day': 12,
+    'Quarter': 1
+}])
+
+sample_input_data[highly_skewed] = sample_input_data[highly_skewed].apply(lambda x: np.log1p(x))
+
+features_for_pt = ['Store', 'Temperature', 'Fuel_Price', 'CPI', 'Month', 'Quarter', 'Year', 'Day']
+sample_input_data[features_for_pt] = pt_features.transform(sample_input_data[features_for_pt])
+sample_input_data = sample_input_data[X_train.columns]
+predicted_sales_transformed = rf.predict(sample_input_data)
+
+original_scale_prediction = pt_target.inverse_transform(predicted_sales_transformed.reshape(-1, 1))[0][0]
+
+print(f"Predicted Weekly Sales (Original Scale): {original_scale_prediction:.2f}")
+
